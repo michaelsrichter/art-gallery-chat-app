96741795d589aa9514c88d0a8c4486735cbf9988
Mike Richter <michaelsrichter@hotmail.com>
2024-11-07 15:00:08 -0500

    Refactor App.js to improve layout and add informational content about Elasticsearch integration with Azure OpenAI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import './custom.css';
-import './App.css';
+import "./custom.css";
+import "./App.css";
 
-import Chatbot from 'react-chatbot-kit'
-import 'react-chatbot-kit/build/main.css'
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
 
-import config from './chatbot/config.js';
-import MessageParser from './chatbot/MessageParser';
-import ActionProvider from './chatbot/ActionProvider';
-
+import config from "./chatbot/config.js";
+import MessageParser from "./chatbot/MessageParser";
+import ActionProvider from "./chatbot/ActionProvider";
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header">
-    <Chatbot
-        config={config}
-        messageParser={MessageParser}
-        actionProvider={ActionProvider}
-      />
-    </header>
-  </div>
+      <header className="App-header">
+      <div className="App-info">
+        <p>
+          This is a demo app that demonstrates the features of Elasticsearch
+          integration with Azure OpenAI.
+        </p>
+        <p>
+          For more information and to see the source code, visit{" "}
+          <a
+            href="https://aka.ms/art-chat-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            this link
+          </a>
+          .
+        </p>
+      </div>
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
+      </header>
+    </div>
   );
 }
 
